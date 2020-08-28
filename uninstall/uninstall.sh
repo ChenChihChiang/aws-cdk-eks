d28da1c464bc3ba1469751bcf1a665f1427351f1
John.Chen <john.chen@grindr.com>
2020-08-28 22:24:46 +0800

    istio

--- uninstall/uninstall.sh
+++ uninstall/uninstall.sh
@@ -16,14 +16,10 @@ kubectl delete namespace istio-system
 
 # delete ECR repo, EKS cluster & VPC
 ## CDK ECR (CloudFormation) is not support --force options in delete repository
-aws ecr batch-delete-image --repository-name jenkins-workshop-go-app --image-ids imageTag=0.0.1
-aws secretsmanager delete-secret --secret-id slack-token-aws --force-delete-without-recovery
-## the secrets manager deletion is an asynchronous process. There might be a short delay.
-sleep 25
-aws secretsmanager describe-secret --secret-id 'slack-token-aws'
+
 
 export EKS_ADMIN_IAM_USERNAME=`aws sts get-caller-identity | jq '.Arn' | cut -d '"' -s -f2`
 
 cd ../01-install-eks-cluster
-cdk destroy -f vpc-stack jenkins-workshop-eks-cluster ecr-repository
+cdk destroy -f vpc-stack istio-eks-cluster ecr-repository
 cd ../uninstall

b827bfbbebfd7bf1a015bda903cb84ea84973d52
Chih Chiang Chen <chihchinag@gmail.com>
2020-09-06 13:55:42 +0800

    Update install.sh

--- 02-install-istio-jenkins/install.sh
+++ 02-install-istio-jenkins/install.sh
@@ -14,7 +14,7 @@ rm -r helm-v2.16.1-linux-amd64.tar.gz  linux-amd64/
 
 EKS_CLUSTER_NAME=`aws eks list-clusters | grep mesh-eks | cut -d '"' -s -f2`
 EKS_ADMIN_ARN=`aws iam list-roles | grep eksctl-mesh-eks-cluster  | grep Arn | cut -d'"' -s -f4`
-EKS_CLUSTER_ARN=`aws eks describe-cluster --name $EKS_CLUSTER_NAME | jq '.cluster.arn' | cut -d '"' -s -f2`
+EKS_CLUSTER_ARN=`aws eks describe-cluster --name=$EKS_CLUSTER_NAME | jq '.cluster.arn' | cut -d '"' -s -f2`
 
 aws eks update-kubeconfig --region ap-northeast-1 --name $EKS_CLUSTER_NAME --role-arn $EKS_ADMIN_ARN
 

40fbb8bc776688b3f335bb6cdb41d8a43dc0416f
Chih Chiang Chen <chihchinag@gmail.com>
2020-09-06 13:03:22 +0800

    Update eks_install.sh

--- 01-install-eks-cluster/eks_install.sh
+++ 01-install-eks-cluster/eks_install.sh
@@ -3,8 +3,14 @@
 set -e
 set -x
 
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 chmod 755 /tmp/eksctl
 
 /tmp/eksctl create cluster -f cluster.yaml

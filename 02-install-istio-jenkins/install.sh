2d142fb548e078df1a318a79e3bf195656edfc01
Chih Chiang Chen <chihchinag@gmail.com>
2020-09-06 13:03:55 +0800

    Update install.sh

--- 02-install-istio-jenkins/install.sh
+++ 02-install-istio-jenkins/install.sh
@@ -10,12 +10,6 @@ tar -zxvf helm-v2.16.1-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
 rm -r helm-v2.16.1-linux-amd64.tar.gz  linux-amd64/
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
-
 # get aws eks kube-config
 
 EKS_CLUSTER_NAME=`aws eks list-clusters | grep mesh-eks | cut -d '"' -s -f2`

12bf9426963df552be8f714e4ef1550b08616e52
Chih Chiang Chen <chihchinag@gmail.com>
2020-11-09 16:25:00 +0800

    Create install_helm3.sh

---
+++ 02-install-jenkins/install_helm3.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# install helm & kubectl cli
+
+wget https://get.helm.sh/helm-v3.4.0-linux-amd64.tar.gz
+tar -zxvf helm-v3.4.0-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+rm -r helm-v3.4.0-linux-amd64.tar.gz  linux-amd64/
+
+# get aws eks kube-config
+
+EKS_CLUSTER_NAME=`aws eks list-clusters | grep linc-eks | cut -d '"' -s -f2`
+EKS_ADMIN_ARN=`aws iam list-roles | grep eksctl-linc-eks-cluster  | grep Arn | cut -d'"' -s -f4`
+EKS_CLUSTER_ARN=`aws eks describe-cluster --name=$EKS_CLUSTER_NAME | jq '.cluster.arn' | cut -d '"' -s -f2`
+
+aws eks update-kubeconfig --region ap-northeast-1 --name $EKS_CLUSTER_NAME
+
+kubectl config use-context $EKS_CLUSTER_ARN
+
+# install jenkins
+
+helm repo add jenkins https://charts.jenkins.io
+
+kubectl create ns jenkins
+
+helm install jenkins --namespace jenkins  jenkins/jenkins
+
+wget https://raw.githubusercontent.com/jenkinsci/helm-charts/main/charts/jenkins/values.yaml
+
+helm diff upgrade  jenkins -f ./values.yaml --namespace jenkins  jenkins/jenkins
